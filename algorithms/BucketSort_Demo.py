04d9db39beea061d3f6410a762435f821a5f1e7d
Enrique A. <98858146+E-A-5@users.noreply.github.com>
2025-09-24 16:02:06 -0700

    Update BucketSort_Demo.py
    remove demos and other main functions

--- algorithms/BucketSort_Demo.py
+++ algorithms/BucketSort_Demo.py
@@ -28,7 +28,5 @@ def bucket_sort(arr: List[float]) -> List[float]:
     return result
 
 
-# Demo
-gpas = [0.78, 0.17, 0.39, 0.26, 0.72, 0.94, 0.99, 0.68]
-print("Unsorted GPAs:", gpas)
-print("Sorted GPAs:", bucket_sort(gpas))
+
+
